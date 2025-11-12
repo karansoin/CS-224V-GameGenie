9d0688c56b5b6141171505448a4ea77bb8072545
karansoin <64022954+karansoin@users.noreply.github.com>
2025-11-11 17:13:02 -0800

    initial exa API setup, with basic search returning serialized + formatted JSON results

---
+++ setup.py
@@ -0,0 +1,8 @@
+import os
+import exa_py
+
+# Option 1: Set API key as environment variable
+os.environ["EXA_API_KEY"] = "dfee28a8-09be-4da2-a70c-729cda3bbd3c"
+
+# Create reusable client
+exa_client = exa_py.Exa(api_key=os.environ["EXA_API_KEY"])
