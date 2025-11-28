00c0d30f3f4a2de92ca24dac7929e49834a82cc6
Justin Hall <hall.justinanthony@gmail.com>
2025-11-28 16:13:51 -0500

    First initial Genie Worksheet Notebook Commit

--- main.py
+++
@@ -1,4 +0,0 @@
-from exa_test import search_and_save_json
-
-# Save search results to JSON
-search_and_save_json("celeste game tips and tricks", "output/search_results.json")
--- setup.py
+++
@@ -1,7 +0,0 @@
-import os
-import exa_py
-
-os.environ["EXA_API_KEY"] = "dfee28a8-09be-4da2-a70c-729cda3bbd3c"
-
-# Create reusable client
-exa_client = exa_py.Exa(api_key=os.environ["EXA_API_KEY"])
